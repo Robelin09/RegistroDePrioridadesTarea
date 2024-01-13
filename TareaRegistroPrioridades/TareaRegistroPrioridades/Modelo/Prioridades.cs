af2df9e9a980f31100beb78061ba2df28dd80a9f
Robelin09 <concepcionrobelin@gmail.com>
2024-01-12 21:58:02 -0400

    Se creo la carpeta modelo con la clase prioridades

--- TareaRegistroPrioridades/TareaRegistroPrioridades/Modelo/Prioridades.cs
+++ TareaRegistroPrioridades/TareaRegistroPrioridades/Modelo/Prioridades.cs
@@ -1,6 +1,14 @@
-﻿namespace TareaRegistroPrioridades.Modelo
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TareaRegistroPrioridades.Modelo
 {
     public class Prioridades
     {
+        [Key]
+        public int PrioridadId { get; set; }
+        [Required(ErrorMessage = "Este campo Obligatorio")]
+        public string? Descripción { get; set; }
+        [Range(1, 31, ErrorMessage = "Valores Validos entre 1 y 31")]
+        public int DiasCompromiso { get; set; }
     }
 }
