c760d0c74cc93c61cdb2403d6eac2f2bcbf3e03d
Robelin09 <concepcionrobelin@gmail.com>
2024-01-20 14:32:50 -0400

    Se creo la BLL Clientes

--- TareaRegistroPrioridades/TareaRegistroPrioridades/Modelo/Clientes.cs
+++ TareaRegistroPrioridades/TareaRegistroPrioridades/Modelo/Clientes.cs
@@ -5,12 +5,11 @@ namespace TareaRegistroPrioridades.Modelo
     public class Clientes
     {
         [Key]
-
         public int ClienteId { get; set; }
 
         [StringLength(70, ErrorMessage = "No se puede exceder el limite de 70 caracteres")]
         [Required(ErrorMessage = "Este campo Obligatorio")]
-        public string? Nombres { get; set; }
+        public string Nombres { get; set; }
 
         [StringLength(10, ErrorMessage = "No se puede exceder el limite de 10 caracteres numericos")]
         [Required(ErrorMessage = "Este campo Obligatorio")]
@@ -26,10 +25,10 @@ namespace TareaRegistroPrioridades.Modelo
 
         [StringLength(50, ErrorMessage = "No se puede exceder el limite de 50 caracteres")]
         [Required(ErrorMessage = "Este campo Obligatorio")]
-        public string? Email { get; set; }
+        public string Email { get; set; }
 
         [StringLength(100, ErrorMessage = "No se puede exceder el limite de 100 caracteres")]
         [Required(ErrorMessage = "Este campo Obligatorio")]
-        public string? Direccion { get; set; }
+        public string Direccion { get; set; }
     }
 }
