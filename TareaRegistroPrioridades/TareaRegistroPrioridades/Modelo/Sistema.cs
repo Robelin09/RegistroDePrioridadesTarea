9d4aa0d038f181090c1ce08a970752ee11da84c3
Robelin09 <concepcionrobelin@gmail.com>
2024-01-29 19:57:42 -0400

    Creé la clase Sistema en el Modelo

---
+++ TareaRegistroPrioridades/TareaRegistroPrioridades/Modelo/Sistema.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TareaRegistroPrioridades.Modelo
+{
+    public class Sistema
+    {
+        [Key]
+        public int SistemaId { get; set; }
+
+        [Required(ErrorMessage = "El Nombre del sistema es obligatorio.")]
+        public string NombredeSistema { get; set; }
+    }
+}
