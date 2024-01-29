c21c29a38553a3b0c7fae6f3e4e484ccdd942e4b
Robelin09 <concepcionrobelin@gmail.com>
2024-01-29 08:35:45 -0400

    Cambié el termino BLL por SERVICES

---
+++ TareaRegistroPrioridades/TareaRegistroPrioridades/SERVICES/ClientesSERVICES.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TareaRegistroPrioridades.DAL;
+using TareaRegistroPrioridades.Modelo;
+
+namespace TareaRegistroPrioridades.BLL
+{
+    public class ClientesSERVICES
+    {
+        private readonly Contexto _contexto;
+
+        public ClientesSERVICES(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+        public bool Existe(int ClienteId)
+        {
+            return _contexto.Clientes.Any(p => p.ClienteId == ClienteId);
+        }
+        public bool Insertar(Clientes Clientes)
+        {
+            _contexto.Clientes.Add(Clientes);
+            return _contexto.SaveChanges() > 0;
+        }
+        public bool Modificar(Clientes Clientes)
+        {
+            var p = _contexto.Clientes.Find(Clientes.ClienteId);
+            _contexto.Entry(p!).State = EntityState.Detached;
+            _contexto.Entry(Clientes).State = EntityState.Modified;
+            return _contexto.SaveChanges() > 0;
+        }
+        public bool Guardar(Clientes Clientes)
+        {
+            if (!Existe(Clientes.ClienteId))
+                return this.Insertar(Clientes);
+            else
+                return this.Modificar(Clientes);
+        }
+        public bool Eliminar(Clientes Clientes)
+        {
+            var p = _contexto.Clientes.Find(Clientes.ClienteId);
+            _contexto.Entry(p!).State = EntityState.Detached;
+            _contexto.Entry(Clientes).State = EntityState.Deleted;
+            return _contexto.SaveChanges() > 0;
+        }
+        public Clientes? Buscar(int ClienteId)
+        {
+            return _contexto.Clientes
+                .Where(t => t.ClienteId == ClienteId)
+                .AsNoTracking()
+                .SingleOrDefault();
+        }
+        public List<Clientes> Listar(Expression<Func<Clientes, bool>> Criterio)
+        {
+            return _contexto.Clientes
+                    .Where(Criterio)
+                    .AsNoTracking()
+                    .ToList();
+        }
+    }
+}
---
+++ TareaRegistroPrioridades/TareaRegistroPrioridades/SERVICES/PrioridadSERVICES.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TareaRegistroPrioridades.DAL;
+using TareaRegistroPrioridades.Modelo;
+
+namespace TareaRegistroPrioridades.BLL
+{
+    public class PrioridadSERVICES
+    {
+        private readonly Contexto _contexto;
+
+        public PrioridadSERVICES(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+        public bool Existe(int RegistroPrioridadId)
+        {
+            return _contexto.Prioridades.Any(p => p.PrioridadId == RegistroPrioridadId);
+        }
+        public bool Insertar(Prioridades Prioridades)
+        {
+            _contexto.Prioridades.Add(Prioridades);
+            return _contexto.SaveChanges() > 0;
+
+        }
+        public bool Modificar(Prioridades Prioridades)
+        {
+            var p = _contexto.Prioridades.Find(Prioridades.PrioridadId);
+            _contexto.Entry(p!).State = EntityState.Detached;
+            _contexto.Entry(Prioridades).State = EntityState.Modified;
+            return _contexto.SaveChanges() > 0;
+        }
+        public bool Guardar(Prioridades Prioridades)
+        {
+            if (!Existe(Prioridades.PrioridadId))
+                return this.Insertar(Prioridades);
+            else
+                return this.Modificar(Prioridades);
+        }
+        public bool Eliminar(Prioridades Prioridades)
+        {
+            var p = _contexto.Prioridades.Find(Prioridades.PrioridadId);
+            _contexto.Entry(p!).State = EntityState.Detached;
+            _contexto.Entry(Prioridades).State = EntityState.Deleted;
+            return _contexto.SaveChanges() > 0;
+        }
+        public Prioridades? Buscar(int PrioridadId)
+        {
+            return _contexto.Prioridades
+                .Where(t => t.PrioridadId == PrioridadId)
+                .AsNoTracking()
+                .SingleOrDefault();
+        }
+        public List<Prioridades> Listar(Expression<Func<Prioridades, bool>> Criterio)
+        {
+            return _contexto.Prioridades
+                    .Where(Criterio)
+                    .AsNoTracking()
+                    .ToList();
+        }
+    }
+}
